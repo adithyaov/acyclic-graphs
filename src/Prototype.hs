d6b5f6fa871b58ca1c8dd062543a5e455250f38a
adithyaov <111501017@smail.iitpkd.ac.in>
2019-03-24 07:19:29 +0530

    Prototype implementation comleted, need to start explaining the model (documentation).

--- src/Prototype.hs
+++ src/Prototype.hs
@@ -16,3 +16,15 @@ circut (x:xs) = foldr overlay empty $ zipWith connect (x : xs) (xs ++ [x])
 
 acyclicGraph :: AcyclicRelation (SimpleOrder (Relation String))
 acyclicGraph = unsafeConvertToAcyclic graph
+
+-- The functions which result in an acyclic graph should
+-- be described like such,
+-- Assuming the type signature of scc is as follows,
+-- scc :: Relation a -> Relation b
+-- scc' can be implemented like follows,
+-- scc' = unsafeConvertToAcyclic . scc
+-- Then the type signature of scc' would be like,
+-- scc' :: Relation a -> AcyclicRelation (SimpleOrder b)
+
+
+
