e8227f0ae6d2cc4946b1ef5e52c6164e11c9bd8c
adithyaov <111501017@smail.iitpkd.ac.in>
2019-03-31 04:57:58 +0530

    Epanded vertices and back to indexed vertices

--- implementation2/AcyclicMonad.hs
+++ implementation2/AcyclicMonad.hs
@@ -6,26 +6,29 @@ type Vertex = Int
 
 -- eg. Edges 4 [2, 3] (Edges 3 [1] (Edges 2 [1] (Edges 1 [] Nil)))
 -- represents 4 * 2 + 4 * 3 + 3 * 1 + 2 * 1 + 1
-data DAG a
-  = Cons a
-         [DAG a]
-         (DAG a)
+data DAG
+  = Cons Vertex
+         [DAG]
+         DAG
   | Nil
   deriving (Show)
 
+-- A simple helper function
+vertex (Cons i _ _) = i
+vertex Nil = 0
+
 -- A State monad creating a singleton
-singleton a = modify (Cons a []) >> get
+singleton = modify (Cons (1 + vertex s) []) >> get
 
 -- A State monad resulting in proper edges
-edgeTo a es = modify (Cons a es) >> get
+edgeTo es = modify (Cons (1 + vertex s) es) >> get
 
 -- A simple function to run the state to get DAG in return
 dag = snd . flip runState Nil
 
--- The result : DAG 3 [1,2] (DAG 2 [] (DAG 1 [] Nil))
+-- The result : DAG 3 [1..DAG,2..DAG] (DAG 2 [] (DAG 1 [] Nil))
 dagTest =
   dag $ do
-    v1 <- singleton 1
-    v2 <- singleton 2
-    3 `edgeTo` [v1, v2]
-
+    v1 <- singleton
+    v2 <- singleton
+    edgeTo [v1, v2]
